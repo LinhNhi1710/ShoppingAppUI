30a666e6f50801a0fa8626863e8b9c0d610fa5ab
Linh Nhi <nhi.nguyen200213@vnuk.edu.vn>
2022-06-10 09:58:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shoppingappui/activity/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.shoppingappui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+import android.widget.ViewFlipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.shoppingappui.R;
+import com.example.shoppingappui.adapter.LoaispAdapter;
+import com.example.shoppingappui.model.Loaisp;
+import com.example.shoppingappui.ultil.CheckConnection;
+import com.example.shoppingappui.ultil.Server;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewmanhinhchinh;
+    NavigationView navigationView;
+    ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
+    ArrayList<Loaisp> mangloaisp;
+    LoaispAdapter loaispAdapter;
+    int id = 0;
+    String tenloaisp = "";
+    String hinhanhloaisp = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AnhXa();
+        ActionBar();
+        ActionViewFlipper();
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionBar();
+            ActionViewFlipper();
+            GetDuLieuLoaisp();
+        }else{
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Bạn hãy kiểm tra kết nối");
+            finish();
+        }
+
+    }
+
+
+    private void GetDuLieuLoaisp() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.DuongdanLoaisp, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response != null) {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            id = jsonObject.getInt("id");
+                            tenloaisp = jsonObject.getString("tenloaisp");
+                            hinhanhloaisp = jsonObject.getString("hinhanhloaisp");
+                            mangloaisp.add(new Loaisp(id, tenloaisp, hinhanhloaisp));
+                            loaispAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mangloaisp.add(3, new Loaisp(0, "Liên Hệ", "link hình ảnh ấy nhá"));
+                    mangloaisp.add(4, new Loaisp(0, "Thông tin", "link hình ảnh ấy nhá"));
+                }
+
+            }
+
+        },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                CheckConnection.ShowToast_Short(getApplicationContext(),error.toString());
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
+
+
+    }
+
+
+
+    private void ActionViewFlipper() {
+        ArrayList<Integer> ads = new ArrayList<>();
+        ads.add(R.drawable.gru_whiteboard_meme);
+        ads.add(R.drawable.jacksepticeye_whiteboard_meme);
+        ads.add(R.drawable.jontrondoctor_meme_format);
+        ads.add(R.drawable.lisa_simpson_presentation_memes);
+        for (int i = 0; i < ads.size(); i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.with(getApplicationContext()).load(ads.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_in = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_in_right);
+        Animation animation_slide_out = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_out_right);
+        viewFlipper.setInAnimation(animation_slide_in);
+        viewFlipper.setOutAnimation(animation_slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+    }
+
+    private void AnhXa() {
+        toolbar =  (Toolbar) findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = (ViewFlipper)  findViewById((R.id.viewlipper));
+        recyclerViewmanhinhchinh = (RecyclerView) findViewById(R.id.recycleview);
+        navigationView = (NavigationView) findViewById((R.id.navigationview));
+        listViewmanhinhchinh = (ListView) findViewById((R.id.listviewmanhinhchinh));
+        mangloaisp = new ArrayList<>();
+        mangloaisp.add(0, new Loaisp(0, "Trang Chinh", ""));
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        loaispAdapter = new LoaispAdapter(mangloaisp, getApplicationContext());
+        listViewmanhinhchinh.setAdapter(loaispAdapter);
+
+//        binding.rvMainPage
+//        binding.nvMainPage
+//        binding.
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppingappui/ultil/Server.java
@@ -0,0 +1,7 @@
+package com.example.shoppingappui.ultil;
+
+public class Server {
+    public static String localhost = "192.168.1.111";
+    public static String DuongdanLoaisp = "http://" + localhost + "/sever/getloaisp.php";
+
+}
